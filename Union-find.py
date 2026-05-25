3ef6e23ff6c13946e9da6f97820dcf4191ae0ae3
Daniel-Espinoza-2121 <167147817+Daniel-Espinoza-2121@users.noreply.github.com>
2026-05-25 02:11:39 -0600

    Se agregan un par de notas

--- Union-find.py
+++ Union-find.py
@@ -5,7 +5,7 @@ def make_set(v):
 def find_set(v):
     if v == parent[v]:
         return v
-    parent[v] = find_set(parent[v])
+    parent[v] = find_set(parent[v]) //Compresion path compresion
     return parent[v]
 
 def swap(a, b):
@@ -16,7 +16,7 @@ def union_sets(a, b):
     a = find_set(a)
     b = find_set(b)
     if a != b:
-        if rank[a] < rank[b]:
+        if rank[a] < rank[b]: #Optimización: union by rank
             a, b = swap(a,b)
 
         parent[b] = a
@@ -29,30 +29,30 @@ def detectarCiclosGrafo(vert, aris):
         make_set(i)
 
     for i, j in aris:
-        if find_set(i) == find_set(j):
+        if find_set(i) == find_set(j): #Si llega a existir un grafo se presentara la situación en la que se encontraran dos elementos del mismo conjunto
             return True
         else:
             union_sets(i, j)
     return False
 
 def main():
+    #Creamos diccionarios y el grafo
     global parent, rank
     parent = {}
     rank = {}
-
     vertices = (1, 2, 3, 4)
     aristas = ((1,2),(2,3),(3,4),(1,3))
 
     resultado = detectarCiclosGrafo(vertices, aristas)
-    print(resultado)
+    print("Existe un ciclo en el grafo:", resultado)
 
+    #Reiniciamos los diccionarios y el grafo
     parent = {}
     rank = {}
-
     aristas = ((1,2),(2,3),(3,4))
 
     resultado = detectarCiclosGrafo(vertices, aristas)
-    print(resultado)
+    print("Existe un ciclo en el grafo:", resultado)
 
 if __name__ == "__main__":
     main()
